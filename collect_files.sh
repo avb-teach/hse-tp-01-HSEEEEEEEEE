617dd1fb98b605fd86844f5bf963625812ba2bac
HSEEEEEEEEE <ekzamenator.help@gmail.com>
2025-04-26 20:43:46 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-python3 "$(dirname "$0")/collect_files.py" "$@"
+set -e
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $(basename "$0") <input_dir> <output_dir>"
+  exit 1
+fi
+
+python3 "$(dirname "$0")/collect_files.py" "$1" "$2"
